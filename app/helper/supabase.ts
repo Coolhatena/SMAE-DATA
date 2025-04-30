18d79366875a4d6a654d920dc8606105cb0d8d87
Coolhatena <ingsc.geffroy@gmail.com>
2025-04-29 20:40:20 -0700

    Remove mock api key

--- app/helper/supabase.ts
+++ app/helper/supabase.ts
@@ -2,7 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
 const url = "https://tkvetehxdzehwhgyqmlp.supabase.co";
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdmV0ZWh4ZHplaHdoZ3lxbWxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NjU3MTIsImV4cCI6MjA2MTU0MTcxMn0.lEKE7KsqKlKhJGe3IkyIQgJekwydAvA33ixQZG3zqlU";
+const key = "";
 
 export const supabase = createClient(url, key, {
 	auth: {
