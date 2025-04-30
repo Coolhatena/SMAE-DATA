19ec78008c39c7b41f02f42151dac02051a858af
Coolhatena <ingsc.geffroy@gmail.com>
2025-04-29 19:54:40 -0700

    NIT: Clean code

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 
 const url = "https://tkvetehxdzehwhgyqmlp.supabase.co/rest/v1/smae";
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdmV0ZWh4ZHplaHdoZ3lxbWxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NjU3MTIsImV4cCI6MjA2MTU0MTcxMn0.lEKE7KsqKlKhJGe3IkyIQgJekwydAvA33ixQZG3zqlU"
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdmV0ZWh4ZHplaHdoZ3lxbWxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NjU3MTIsImV4cCI6MjA2MTU0MTcxMn0.lEKE7KsqKlKhJGe3IkyIQgJekwydAvA33ixQZG3zqlU";
 
 export const supabase = createClient(url, key, {
 	auth: {
@@ -11,4 +11,4 @@ export const supabase = createClient(url, key, {
 		persistSession: true,
 		detectSessionInUrl: true,
 	}
-})
+});
