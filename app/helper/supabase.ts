ede1200840835428c3b1c2853744038cddbb44ca
Coolhatena <ingsc.geffroy@gmail.com>
2025-04-30 15:35:26 -0700

    Add key from .env

--- app/helper/supabase.ts
+++ app/helper/supabase.ts
@@ -2,9 +2,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
 const url = "https://tkvetehxdzehwhgyqmlp.supabase.co";
-const key = "";
+const key = process.env.EXPO_PUBLIC_SUPABASE_API_KEY;
 
-export const supabase = createClient(url, key, {
+export const supabase = createClient(url, key!, {
 	auth: {
 		storage: AsyncStorage,
 		autoRefreshToken: true,
