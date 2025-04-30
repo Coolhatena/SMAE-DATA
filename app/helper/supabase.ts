40cdfc194852ee3a3049986ad8ba6a8a63ea7553
Coolhatena <ingsc.geffroy@gmail.com>
2025-04-30 15:42:47 -0700

    Change mock uri for .env uri

--- app/helper/supabase.ts
+++ app/helper/supabase.ts
@@ -1,10 +1,10 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const url = "https://tkvetehxdzehwhgyqmlp.supabase.co";
+const url = process.env.EXPO_PUBLIC_SUPABASE_URI;
 const key = process.env.EXPO_PUBLIC_SUPABASE_API_KEY;
 
-export const supabase = createClient(url, key!, {
+export const supabase = createClient(url!, key!, {
 	auth: {
 		storage: AsyncStorage,
 		autoRefreshToken: true,
