e1578827d73e4b72782b09e1b03166e66f4c7e9d
Coolhatena <ingsc.geffroy@gmail.com>
2025-04-29 20:05:58 -0700

    Create SMAE interface

---
+++ app/interfaces/smaeInterface.ts
@@ -0,0 +1,31 @@
+export interface SmaeInterface {
+	id:                   string;
+	categoria:            string;
+	alimento:             string;
+	cantidad:             string;
+	unidad:               string;
+	peso_bruto:           string;
+	peso_neto:            string;
+	kcal:                 string;
+	proteina:             string;
+	lipidos:              string;
+	carbohidratos:        string;
+	grasa_saturada:       string;
+	grasa_monoinsaturada: string;
+	grasa_poliinsaturada: string;
+	colesterol:           string;
+	azucar:               string;
+	fibra:                string;
+	vitamina_a:           string;
+	vitamina_c:           string;
+	acido_folico:         string;
+	calcio:               string;
+	hierro:               string;
+	potasio:              string;
+	sodio:                string;
+	fosforo:              string;
+	selenio:              string;
+	etanol:               string;
+	IG:                   string;
+	IC:                   string;
+}
