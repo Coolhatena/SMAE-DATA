47d871eaf9ea2ae29d7a25ee42f441abe949518d
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-01 20:29:43 -0700

    Adapt supabase.ts to use expo-secure-store

--- app/helper/supabase.ts
+++ app/helper/supabase.ts
@@ -1,12 +1,31 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import 'react-native-url-polyfill';
+import * as SecureStore from "expo-secure-store";
 import { createClient } from "@supabase/supabase-js";
 
 const url = process.env.EXPO_PUBLIC_SUPABASE_URI;
 const key = process.env.EXPO_PUBLIC_SUPABASE_API_KEY;
 
+interface StorageAdapter { 
+	getItem: (key: string) => Promise<string | null>;
+	setItem: (key: string, value: string) => Promise<void>;
+	removeItem: (key: string) => Promise<void>;
+}
+
+const ExpoSecureStoreAdapter: StorageAdapter = {
+	getItem: (key: string) => {
+		return SecureStore.getItemAsync(key);
+	},
+	setItem: (key: string, value: string) => {
+		return SecureStore.setItemAsync(key, value);
+	},
+	removeItem: (key: string) => {
+		return SecureStore.deleteItemAsync(key);
+	}
+}
+
 export const supabase = createClient(url!, key!, {
 	auth: {
-		storage: AsyncStorage,
+		storage: ExpoSecureStoreAdapter,
 		autoRefreshToken: true,
 		persistSession: true,
 		detectSessionInUrl: true,
