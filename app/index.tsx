b92233ac4567477b149e99e3d1f8738218526f7a
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-02 01:17:20 -0700

    Use index.tsx as a redirect to auth or app

--- app/index.tsx
+++ app/index.tsx
@@ -1,66 +1,24 @@
-import { useEffect, useState } from "react";
-import { Button, FlatList, Text, View } from "react-native";
-import { supabase } from "../lib/supabase";
-import SmaeInterface from "./interfaces/smaeInterface";
+import { useEffect } from 'react';
+import { Redirect } from 'expo-router';
+import { useAuth } from '../lib/auth';
+import { SplashScreen } from 'expo-router';
 
 export default function Index() {
-  const [alimentos, setAlimentos] = useState<SmaeInterface[]>([]);
-  const [dataError, setDataError] = useState("");
+  const { user, loading } = useAuth();
 
   useEffect(() => {
-    loadAlimentos();
-  }, []);
-
-  const loadAlimentos = async () => {
-    try {
-      const { data, error } = await supabase.from("smae").select("*").limit(4);
-      if (error) {
-        setDataError(error.message);
-        console.log("Supabase error:", error.details);
-      } else {
-        setAlimentos(data ?? []);
-      }
-    } catch (error) {
-      console.log("Network or unexpected error:", error);
+    if (!loading) {
+      SplashScreen.hideAsync();
     }
-  };
+  }, [loading]);
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {alimentos.length !== 0 ? (
-        <FlatList
-          ListHeaderComponent={
-            <View style={{ marginBottom: 20 }}>
-              <Text style={{ fontSize: 25, textTransform: "uppercase" }}>
-                Lista de alimentos
-              </Text>
-              <Text>Búsqueda básica</Text>
-            </View>
-          }
-          keyExtractor={(item) => item.id}
-          data={alimentos}
-          renderItem={({ item }) => (
-            <Text style={{ fontSize: 20, fontWeight: "thin" }}>
-              {item.alimento}
-            </Text>
-          )}
-        />
-      ) : (
-        <>
-          <Text>Error</Text>
-          <Text>{dataError}</Text>
-        </>
-      )}
-    </View>
-  );
+  if (loading) {
+    return null;
+  }
+
+  if (user) {
+    return <Redirect href="/(app)" />;
+  } else {
+    return <Redirect href="/(auth)/login" />;
+  }
 }
-
-export const options = {
-	title: "SMAE",
-  };
